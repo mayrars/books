94d07dbca29a9a073e76f64a0ebd99b307e64086
mayrars <mayra203@gmail.com>
2025-02-10 07:11:41 -0600

    created home and about page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div className="text-black">
+        <h1>home</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe repellendus distinctio ea facilis harum rerum incidunt minima maiores quam aperiam. Optio, modi itaque. Nobis totam corrupti, dolor veritatis sit vitae.</p>
+    </div>
+  )
+}
+
+export default Home
