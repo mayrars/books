ba1070d8dc8e32c485b812ab1cf510734d3bede9
mayrars <mayra203@gmail.com>
2025-02-09 19:07:11 -0600

    created footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <div style={{height: '75vh'}}></div>
+      <Footer />
     </>
   )
 }
