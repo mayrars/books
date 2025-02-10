14812a915977337e0c6cdeb4584635c97844ce49
mayrars <mayra203@gmail.com>
2025-02-10 07:11:21 -0600

    change created routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router'
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <>
-      <Header />
-      <div style={{height: '75vh'}}></div>
-      <Footer />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
