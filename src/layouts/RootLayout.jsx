ed542789d94d71083255ac8db983ed56a256395e
mayrars <mayra203@gmail.com>
2025-02-11 07:09:23 -0600

    import footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const RootLayout = () => {
   return (
     <div>
         <Header />
-        <main>
+        <main style={{ height: 'calc(100vh - 327px)'}}>
             <Outlet />
         </main>
+        <Footer />
     </div>
   )
 }
