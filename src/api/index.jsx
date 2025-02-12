c0ace8a81c8dbdbe44cdb3e4e536f1c0b63242dc
mayrars <mayra203@gmail.com>
2025-02-12 07:03:42 -0600

    changed api

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,19 +1,17 @@
-import axios from 'axios'
+import mostPopularbooks from './mostpopularbooks.json'
+import popularbooks from './popularbooks.json'
+import nominatebooks from './nominatebooks.json'
 
-export async function getMostPopularBooks(){
-    const options = {
-        method: 'GET',
-        url: 'https://hapi-books.p.rapidapi.com/month/2024/12',
-        headers: {
-          'x-rapidapi-key': '5429963261msh2a15d87840d5f5bp1468d2jsn265821d0744d',
-          'x-rapidapi-host': 'hapi-books.p.rapidapi.com'
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          console.log(response.data);
-      } catch (error) {
-          console.error(error);
-      }
+function mostPopularBooks() {
+    return mostPopularbooks;
 }
+
+function popularBooks() {
+    return popularbooks;
+}
+
+function nominateBooks() {
+    return nominatebooks;
+}
+
+export { mostPopularBooks, popularBooks, nominateBooks };
