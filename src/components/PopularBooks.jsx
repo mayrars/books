04be135e20cf260f503587750c5856fc6b38c4ac
mayrars <mayra203@gmail.com>
2025-02-12 07:01:09 -0600

    created nominated and popular books

---
+++ src/components/PopularBooks.jsx
@@ -0,0 +1,19 @@
+
+const PopularBooks = ({data}) => {
+  return (
+    <div>
+        <h2 className="text-3xl font-bold mb-4">Popular Books</h2>
+        <div className="grid grid-cols-5 gap-8">
+            {data.map((book) => (
+            <div key={book.id}>
+                <img src={book.cover} alt={book.name} className="w-full h-110 object-cover mb-2"/>
+                <h3 className="text-lg font-medium text-center">{book.name}</h3>
+            </div>  
+            ))}
+        </div>
+    </div>
+   
+  )
+}
+
+export default PopularBooks
