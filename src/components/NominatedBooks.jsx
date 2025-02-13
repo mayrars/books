9b364c2582c18fad8a40261629493e462b25394a
mayrars <mayra203@gmail.com>
2025-02-13 07:11:08 -0600

    imprt styles

--- src/components/NominatedBooks.jsx
+++ src/components/NominatedBooks.jsx
@@ -2,13 +2,35 @@ const NominatedBooks = ({data}) => {
   return (
     <div>
         <h2 className="text-3xl font-bold mb-4 mt-6">Nominated Books</h2>
-        <div className="grid grid-cols-5 gap-8">
+        <div className="grid grid-cols-5 gap-8 mt-20 flex justify-center mb-20">
             {data.map((book) => (
-            <div key={book.id}>
-                <img src={book.cover} alt={book.name} className="w-full h-110 object-cover mb-2"/>
-                <h3 className="text-lg font-medium text-center">{book.name}</h3>
-            </div>  
-            ))}
+              <figure className='book mt-5 mb-10' key={book.id}>
+                <ul className='hardcover_front'>
+                  <li>
+                    <img src={book.cover} alt="" width="100%" height="100%" />
+                  </li>
+                  <li></li>
+                </ul>
+                <ul className='page'>
+                  <li></li>
+                  <li>
+                    <h1 className="text-normal font-bold flex justify-center items-center h-full">{book.name}</h1>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+  
+                <ul className='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul className='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            ))} 
         </div>
     </div>
   )
