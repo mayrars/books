c49850360a021d79d357cb427cd0a191af3b9a7b
mayrars <mayra203@gmail.com>
2025-02-14 06:51:03 -0600

    import book comonent

--- src/components/NominatedBooks.jsx
+++ src/components/NominatedBooks.jsx
@@ -1,35 +1,12 @@
+import Book from "./Book"
+
 const NominatedBooks = ({data}) => {
   return (
     <div>
-        <h2 className="text-3xl font-bold mb-4 mt-6">Nominated Books</h2>
-        <div className="grid grid-cols-5 gap-8 mt-20 flex justify-center mb-20">
+        <h2 className="text-3xl font-bold mb-4 mt-6 text-pink-500 text-shadow-lg">Nominated Books</h2>
+        <div className="grid lg:md:grid-cols-5 grid-cols-1 gap-8 mt-20 flex justify-center mb-20">
             {data.map((book) => (
-              <figure className='book mt-5 mb-10' key={book.id}>
-                <ul className='hardcover_front'>
-                  <li>
-                    <img src={book.cover} alt="" width="100%" height="100%" />
-                  </li>
-                  <li></li>
-                </ul>
-                <ul className='page'>
-                  <li></li>
-                  <li>
-                    <h1 className="text-normal font-bold flex justify-center items-center h-full">{book.name}</h1>
-                  </li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
-  
-                <ul className='hardcover_back'>
-                  <li></li>
-                  <li></li>
-                </ul>
-                <ul className='book_spine'>
-                  <li></li>
-                  <li></li>
-                </ul>
-              </figure>
+              <Book book={book} key={`Nominated-${book.book_id}`} />
             ))} 
         </div>
     </div>
