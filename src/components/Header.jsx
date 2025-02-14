e3fecd7de0cd5fdebd3bc906cd2e3dee34dafbc2
mayrars <mayra203@gmail.com>
2025-02-14 06:50:26 -0600

    deleted link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,9 +44,6 @@ const Header = () => {
                     <NavLink to="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</NavLink>
                 </li>
                 <li>
-                    <NavLink to="about" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Books</NavLink>
-                </li>
-                <li>
                     <NavLink to="authors" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Authors</NavLink>
                 </li>
             </ul>
