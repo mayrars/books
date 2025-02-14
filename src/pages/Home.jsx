b1f6815847276b7debb28d1e25f99d6fc488d81b
mayrars <mayra203@gmail.com>
2025-02-14 06:51:17 -0600

    import most popular books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState, useCallback } from "react"
 import { popularBooks, mostPopularBooks, nominateBooks } from "../api"
 import PopularBooks from "../components/PopularBooks"
 import NominatedBooks from "../components/NominatedBooks"
+import MostPopularBooks from "../components/MostPopularBooks"
 const Home = () => {
   return (
     <div className="text-black">
-      <div className="container mx-auto py-8 ">
+      <div className="container mx-auto lg:md:py-8 p-5">
         <PopularBooks className="mb-10" data={popularBooks()} />
         <NominatedBooks className="mt-10" data={nominateBooks()} />
+        <MostPopularBooks className="mt-10" data={mostPopularBooks()} />
       </div>
     </div>
   )
