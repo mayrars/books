977b22afc51a8fad03f1523da05983a98966080f
mayrars <mayra203@gmail.com>
2025-03-03 05:42:47 -0600

    created new route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import RootLayout from './layouts/RootLayout'
+import BookInfo from './pages/BookInfo'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="book/:bookId" element={<BookInfo />} />
     </Route>
   )
 )
