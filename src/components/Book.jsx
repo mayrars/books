e3ee4e1067493fe699f0f292cf268aac91902b9d
mayrars <mayra203@gmail.com>
2025-03-03 05:43:24 -0600

    adding new link to view book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,28 +5,29 @@ const Book = ({book}) => {
     <div className="flex flex-col items-center justify-start" key={`Popular-${book.book_id}`}>
         <figure className='book mt-5 mb-5'>
             <ul className='hardcover_front'>
-            <li>
-                <img src={book.cover} alt="" width="100%" height="100%" />
-            </li>
-            <li></li>
+              <li>
+                  <img src={book.cover} alt="" width="100%" height="100%" />
+              </li>
+              <li></li>
             </ul>
             <ul className='page'>
-            <li></li>
-            <li className='flex h-full items-center justify-center'>
-                <Link to={book.url} className="text-normal font-bold items-center text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">View more</Link>
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
+              <li></li>
+              <li className='flex h-full items-center justify-center flex flex-col'>
+                <Link to={`/book/${book.book_id}`} className="ext-normal font-bold items-center text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 mb-5">View more</Link>
+                <Link to={book.url} className="text-normal font-bold items-center text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Go shopping</Link>
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
             </ul>
 
             <ul className='hardcover_back'>
-            <li></li>
-            <li></li>
+              <li></li>
+              <li></li>
             </ul>
             <ul className='book_spine'>
-            <li></li>
-            <li></li>
+              <li></li>
+              <li></li>
             </ul>
         </figure>
         <h2 className="text-center font-bold text-2xl mt-5">{book.name}</h2>
