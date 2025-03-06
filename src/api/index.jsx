e9fb95302f19161af03f5ce1ad7b45a3f883919e
mayrars <mayra203@gmail.com>
2025-03-06 07:02:03 -0600

    created search module

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios'
 import mostPopularbooks from './mostpopularbooks.json'
 import popularbooks from './popularbooks.json'
 import nominatebooks from './nominatebooks.json'
 import singlebook from './singlebook.json'
+import mostPopularauthors from './mostpopularauthors.json'
+import authordata from './author.json'
 
 function mostPopularBooks() {
     return mostPopularbooks;
@@ -19,6 +22,30 @@ function singleBook() {
     return singlebook;
 }
 
+function mostPopularAuthors() {
+    return mostPopularauthors;
+}
 
+function authorData() {
+    return authordata
+}
 
-export { mostPopularBooks, popularBooks, nominateBooks, singleBook };
+async function searchByName(text) {
+    const options = {
+    method: 'GET',
+    url: `https://hapi-books.p.rapidapi.com/search/${text}`,
+    headers: {
+        'x-rapidapi-key': '5429963261msh2a15d87840d5f5bp1468d2jsn265821d0744d',
+        'x-rapidapi-host': 'hapi-books.p.rapidapi.com'
+    }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export { mostPopularBooks, popularBooks, nominateBooks, singleBook, mostPopularAuthors, authorData, searchByName };
