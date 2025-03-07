55cd35c139d150df9363c7e1f8f9d48141e23588
mayrars <mayra203@gmail.com>
2025-03-07 07:02:18 -0600

    changed view of search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,16 @@
 
 import { useState } from 'react';
 import { searchByName } from "../api"
+
 const Search = () => {
     const [text, setText] = useState('')
     const [searchResults, setSearchResults] = useState([])
+    const [openSearchResults, setOpenSearchResults] = useState(true)
     async function onSearch(e) {
         setText(e.target.value)
         const data = await searchByName(e.target.value)
         setSearchResults(data)
-        console.log(searchResults)
+        setOpenSearchResults(true)
     }
     return (
         <div className="flex md:order-2">
@@ -19,13 +21,18 @@ const Search = () => {
             <span className="sr-only">Search</span>
             </button>
             <div className="relative hidden md:block">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-                <span className="sr-only">Search icon</span>
-            </div>
-            <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-rose-200 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." value={text} onChange={onSearch}/>
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                    <span className="sr-only">Search icon</span>
+                </div>
+                <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-rose-200 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." value={text} onChange={onSearch}/>
+                <div className={`${openSearchResults ? 'block' : 'hidden'} absolute top-14 right-0 w-full md:w-96 bg-white rounded-lg shadow-lg border border-gray-200 w-full h-80 z-10 overflow-y-scroll`}>
+                        {searchResults.map((result) =>(
+                            <div className="grid grid-cols-2 gap-4" key={result.book_id}></div>
+                        ))}
+                </div>
             </div>
             <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
