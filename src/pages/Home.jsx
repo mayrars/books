c1541591e1ce0f82b55ef6f293b757f61d7a1fad
mayrars <mayra203@gmail.com>
2025-03-11 07:02:30 -0600

    change container background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState, useCallback } from "react"
 import { popularBooks, mostPopularBooks, nominateBooks } from "../api"
 import PopularBooks from "../components/PopularBooks"
 import NominatedBooks from "../components/NominatedBooks"
@@ -8,7 +7,13 @@ const Home = () => {
     <div className="text-black">
       <div className="container mx-auto lg:md:py-8 p-5">
         <PopularBooks className="mb-10" data={popularBooks()} />
-        <NominatedBooks className="mt-10" data={nominateBooks()} />
+      </div>
+      <div className="bg-[#E4F1AC]">
+        <div className="container mx-auto lg:md:py-8 p-5">
+          <NominatedBooks className="mt-10" data={nominateBooks()} />
+        </div>
+      </div>
+      <div className="container mx-auto lg:md:py-8 p-5">
         <MostPopularBooks className="mt-10" data={mostPopularBooks()} />
       </div>
     </div>
